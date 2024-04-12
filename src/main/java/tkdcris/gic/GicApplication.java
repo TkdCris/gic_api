720784d256f1bd7808f07d8884d13d6adbdd6dc4
Cristiano Lopes Duarte <tkdcris@gmail.com>
2024-04-11 23:13:55 -0300

    First commit

---
+++ src/main/java/tkdcris/gic/GicApplication.java
@@ -0,0 +1,13 @@
+package tkdcris.gic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tkdcris/gic/GicApplicationTests.java
@@ -0,0 +1,13 @@
+package tkdcris.gic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
